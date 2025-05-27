30f080fe7da16c30318510158d370d5be691952a
ymer94 <ymer_94@hotmail.com>
2025-05-27 22:08:41 +0200

    Updated global css import.
    :# Please enter the commit message for your changes. Lines starting
    Changes to be committed:
    	modified:   src/routes/+layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import { base } from '$app/paths'
-	import '$css/style.css'
 
 	let { children } = $props()
 </script>
@@ -9,6 +8,7 @@
 	<title>Portfolio - Ymer</title>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="text/css" href="{'$css'}/style.css" />
 </svelte:head>
 
 <header>
