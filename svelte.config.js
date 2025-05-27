c03f0912720f9cda099dcb1c8c2d40189613ac14
ymer94 <ymer_94@hotmail.com>
2025-05-27 19:48:49 +0200

    Initial commit.
    Changes to be committed:
    	new file:   .gitignore
    	new file:   .npmrc
    	new file:   .prettierignore
    	new file:   .prettierrc
    	new file:   README.md
    	new file:   eslint.config.js
    	new file:   package.json
    	new file:   src/app.d.ts
    	new file:   src/index.html
    	new file:   src/lib/index.ts
    	new file:   src/lib/static/.nojekyll
    	new file:   src/lib/static/css/style.css
    	new file:   src/lib/static/favicon.png
    	new file:   src/routes/+layout.svelte
    	new file:   src/routes/+page.svelte
    	new file:   svelte.config.js
    	new file:   tailwind.config.js
    	new file:   tsconfig.json
    	new file:   vite.config.ts

---
+++ svelte.config.js
@@ -0,0 +1,29 @@
+import { mdsvex } from 'mdsvex'
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { base } from '$service-worker';
+
+const libPath = 'src/lib', assetPath = `${libPath}/static`, config = {
+	preprocess: [vitePreprocess(), mdsvex()],
+	kit: {
+		adapter: adapter(),
+		files: {
+            appTemplate: 'src/index.html',
+			assets: assetPath
+        },
+		alias: {
+			$css : `${assetPath}/css`,
+			$img: `${assetPath}/img`,
+			$fonts: `${assetPath}/fonts`,
+			$lib: libPath,
+			$js: `${libPath}/js`,
+			$components: `${libPath}/components`
+		},
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : ''
+		}
+	 },
+	extensions: ['.svelte', '.svx']
+};
+
+export default config
---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+export const plugins = [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+];
