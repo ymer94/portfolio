7800f941a0999b28aaa3f6425c272948db7cc026
ymer94 <ymer_94@hotmail.com>
2025-05-30 17:00:16 +0200

    Added an error page.
    Also enabled auto completion and type checking along with fixing all errors
    that occured along with that.
    
    Changes to be committed:
    	modified:   package.json
    	modified:   src/app.d.ts
    	new file:   src/lib/static/css/error.css
    	new file:   src/lib/static/css/main.css
    	modified:   src/lib/static/css/style.css
    	new file:   src/routes/+error.svelte
    	modified:   src/routes/+layout.svelte
    	modified:   src/routes/about/+page.svelte
    	modified:   svelte.config.js
    	modified:   tsconfig.json

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+    import { base } from '$app/paths'
+    import  'css/error.css'
+
+    const home = `${base}/`
+</script>
+
+<div class="error-container">
+    <h1>{ $page.status }: { $page.error?.message }</h1>
+    <p>Sorry, the page you're looking for doesn't exist.</p>
+    <button type="button" on:click={ () => location.href = home }><a href={ home }>Go back home</a></button>
+</div>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Om mig  - Ymer Nordström</h1>
+
+<script lang="ts">
+    throw new Error("This page is not implemented yet.");
+</script>
