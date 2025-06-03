4b8c20022186e8fdffcbf914f13f700c594bea13
ymer94 <ymer_94@hotmail.com>
2025-06-03 16:19:02 +0200

    Added a short hero section.
    Temporary inntroduction (only AI generated text).
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   src/lib/static/css/main.css
    	modified:   src/lib/static/css/style.css
    	new file:   src/lib/static/img/profile.jpg
    	new file:   src/lib/static/img/profile.png
    	modified:   src/routes/+layout.svelte
    	modified:   src/routes/+page.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <svelte:head>
-	<title>Portfolio - Ymer</title>
+	<title>Portfolio | Ymer</title>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+    <img src="/img/profile.png" alt="Anime style portrait of Ymer Nordström.">
+</div>
+<section>
+    <h1>Ymer Nordström</h1>
+    <h2>Software developer</h2>
+    <p>
+        I am a software developer with a passion for creating innovative solutions. I have experience in various programming languages and frameworks, and I enjoy tackling complex challenges. My goal is to contribute to projects that make a positive impact on users and the world.
+        I am always eager to learn new technologies and improve my skills. Let's connect and explore opportunities to collaborate on exciting projects!
+    </p>
+</section>
+
+<style>
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        & > img {
+            width: 15rem;
+            height: 15rem;
+            border: .25rem solid var(--color-purple-light);
+            border-radius: 50%;
+        }
+    }
+
+    p {
+        max-width: 50em;
+        text-align: justify;
+    }
+</style>
