87d3ec768374e2eb089d9996ca4e06521b467118
ymer94 <ymer_94@hotmail.com>
2025-07-08 11:11:35 +0200

    Fixed some styling issues.
    Changes to be committed:
    	modified:   src/routes/+page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,21 +8,65 @@
     <h1>Ymer Nordström</h1>
     <h2>Software developer</h2>
     <p>
-        I'm a general software developer with a passion for creating customized solutions directly against end users.
-        In my years I have worked in a number of different teams, languages & environments.
+        I'm a general software developer with a passion for creating customized solutions
+        directly against end users. In my years I have worked in a number of different teams,
+        languages & environments.
     </p>
-    <section>
-        <h3>
-            With experience in:
-        </h3>
-        <ul>
-            <li><Icon icon='devicon:html5' />HTML<span>/</span><Icon icon='devicon:php' />PHP <span>(</span><Icon icon='cib:wordpress' />WordPress<span>...</span><span>)</span></li>
-            <li><Icon icon='devicon:css3' />CSS<span>/</span><Icon icon='cib:sass' color='#CD6799' />Sass <span>(</span><Icon icon='devicon:tailwindcss' />Tailwind<span>/</span><Icon icon='devicon:bootstrap' />Bootstrap<span>...</span><span>)</span></li>
-            <li><Icon icon='devicon:javascript' />JS<span>/</span><Icon icon='devicon:typescript' />TS <span>(</span><Icon icon='devicon:react' />React<span>/</span><Icon icon='devicon:nextjs' />Next.js<span>/</span><Icon icon='devicon:svelte' />Svelte<span>...</span><span>)</span></li>
-            <li><Icon icon='devicon:nodejs' />Node.js <span>(</span><Icon icon='devicon:express' />Express, <Icon icon='devicon-plain:axios' color='#5a29e4' />Axios<span>...</span><span>)</span></li>
-            <li><Icon icon='devicon:git' />Git<span>/</span><Icon icon='devicon:github' />GitHub<span>/</span><Icon icon='devicon:gitlab' />GitLab</li>
-        </ul>
-    </section>
+</section>
+<section>
+    <h3>
+        Experienced in:
+    </h3>
+    <ul>
+        <li>            
+            <Icon icon='devicon:html5' />HTML
+            <span>/</span>
+            <Icon icon='devicon:php' />PHP
+            <span>(</span>
+            <Icon icon='cib:wordpress' />WordPress
+            <span>...</span>
+            <span>)</span>
+        </li>
+        <li>
+            <Icon icon='devicon:css3' />CSS
+            <span>/</span>
+            <Icon icon='cib:sass' color='#CD6799' />Sass
+            <span>(</span>
+            <Icon icon='devicon:tailwindcss' />Tailwind
+            <span>/</span>
+            <Icon icon='devicon:bootstrap' />Bootstrap
+            <span>...</span>
+            <span>)</span>
+        </li>
+        <li>
+            <Icon icon='devicon:javascript' />JS
+            <span>/</span>
+            <Icon icon='devicon:typescript' />TS
+            <span>(</span>
+            <Icon icon='devicon:react' />React
+            <span>/</span>
+            <Icon icon='devicon:nextjs' />Next.js
+            <span>/</span>
+            <Icon icon='devicon:svelte' />Svelte
+            <span>...</span>
+            <span>)</span>
+        </li>
+        <li>
+            <Icon icon='devicon:nodejs' />Node.js 
+            <span>(</span>
+            <Icon icon='devicon:express' />Express, 
+            <Icon icon='devicon-plain:axios' color='#5a29e4' />Axios
+            <span>...</span>
+            <span>)</span>
+        </li>
+        <li>
+            <Icon icon='devicon:git'/>Git
+            <span>/</span>
+            <Icon icon='devicon:github' />GitHub
+            <span>/</span>
+            <Icon icon='devicon:gitlab' />GitLab
+        </li>
+    </ul>
 </section>
 
 <style>
@@ -33,16 +77,27 @@
         border-radius: 50%;
     }
 
-    section {
-        > section, p, ul {
-            &:not(:last-child) {
-                margin-bottom: 1em;
-            }
+    section, p, ul {
+        &:not(:last-child) {
+            margin-bottom: 1em;
         }
     }
 
-    p {
+    p, ul {
         max-width: 50em;
+        width: 100%;
+    }
+
+    section {
+        width: 100dvw;
+        padding: 0 2rem;
+
+        @media (width >= 48rem) {
+            width: 75%;
+        }
+    }
+    
+    p {
         font-size: 1.25em;
 
         @media (width >= 48rem) {
@@ -50,9 +105,16 @@
         }
     }
 
-    li {
+    ul {
         display: flex;
-        align-items: center;
-        gap: .5em;
+        flex-direction: column;
+        gap: 1em;
+
+        > li {
+            display: flex;
+            align-items: center;
+            gap: .5em;
+        }
     }
+
 </style>
