2c2bcf777221f244ce2cf395bab3201352d0fad1
ymer94 <ymer_94@hotmail.com>
2025-07-09 15:42:56 +0200

    Added a projects page.
    Changes to be committed:
    	new file:   src/routes/projects/+page.svelte

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,4 @@
+<h1>My projects</h1>
+<p>
+Welcome to my projects page! Here you can find a collection of my work, including web applications, libraries, and other software projects. Each project is a testament to my skills and interests in software development. Feel free to explore and reach out if you have any questions or feedback!
+</p>
