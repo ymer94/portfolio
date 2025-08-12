c5999d66b53b6fa8636da8984f0a7617170ea16c
ymer94 <ymer_94@hotmail.com>
2025-08-12 23:32:35 +0200

    New deployment run.

--- _app/immutable/chunks/vfxCMXp0.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./rKXFrg0d.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}};export{r as g};
---
+++ _app/immutable/entry/start.DrKRVFAv.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/9Y66Zw3v.js";export{o as load_css,r as start};
