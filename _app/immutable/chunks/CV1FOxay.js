bb15993ef07fac04170e941e4988ae536683bfd3
ymer94 <ymer_94@hotmail.com>
2025-08-12 23:27:15 +0200

    New deployment run.

--- _app/immutable/chunks/CV1FOxay.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./BZHMhULM.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}};export{r as g};
---
+++ _app/immutable/chunks/a_k8kdlB.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1jit25t)==null?void 0:s.base)??"/portfolio";var t;const e=((t=globalThis.__sveltekit_1jit25t)==null?void 0:t.assets)??a;export{e as a,a as b};
