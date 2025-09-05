a6923c67532432018e104d01f17900b19dc2ab66
ymer94 <ymer_94@hotmail.com>
2025-09-05 08:28:31 +0200

    New deployment run.

---
+++ _app/immutable/chunks/CVTkMOF0.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_vfarec)==null?void 0:s.base)??"/portfolio";var a;const t=((a=globalThis.__sveltekit_vfarec)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/chunks/Dy11FZ4Q.js
@@ -0,0 +1,1 @@
+import{s as e}from"./BZCtht_Q.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}};export{r as g};
