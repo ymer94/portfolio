1ea256c40623e43119e41c2833244f72a555d01a
ymer94 <ymer_94@hotmail.com>
2026-05-05 16:21:04 +0200

    New deployment run.

---
+++ _app/immutable/chunks/h9loJTdI.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1ydh0s7)==null?void 0:s.base)??"/portfolio";var t;const e=((t=globalThis.__sveltekit_1ydh0s7)==null?void 0:t.assets)??a;export{e as a,a as b};
