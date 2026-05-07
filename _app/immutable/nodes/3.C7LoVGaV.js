09e5a273dd1415dd6b7eef5cc0aed477ffe774be
ymer94 <ymer_94@hotmail.com>
2026-05-07 15:50:58 +0200

    New deployment run.

--- _app/immutable/nodes/3.C7LoVGaV.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/Bzak7iHL.js";import"../chunks/69_IOA4Y.js";import{f as m,a as t}from"../chunks/BG2KHw6v.js";var a=m("<h1>Om mig  - Ymer Nordström</h1>");function n(o){var r=a();t(o,r)}export{n as component};
